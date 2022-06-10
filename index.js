9a4777de4770cdfdd2b9686423de441b2d1ade45
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-10 07:40:00 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+ /*function superBowlWin(record){
+ 
+ let wining=(record)=>record.result === "W";
+    let win =record.find(wining)
+    if (win)  
+    {
+        return win.year;
+    }
+else {
+    return undefined;
+}
+}*/
+
+
+// code your solution here
+function superbowlWin(record){
+    const winning = (record) => record.result === 'W';
+    const win =  record.find(winning)  
+    if (win)
+    {
+        return win.year
+    }
+    else {
+        return undefined;
+    }}
